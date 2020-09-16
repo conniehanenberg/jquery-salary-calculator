ee8d42191d3d14aeac3ef8e5122d68e84ecb6e47
Connie Hanenberg <conniejane66@gmail.com>
2020-09-15 20:54:00 -0500

    final commit

--- client.js
+++ client.js
@@ -1,123 +1,101 @@
 $(document).ready(console.log('hello world'));
 
-$(document).ready(function() {
+$(document).ready(setup);
     console.log('document is ready');
 
- // clear the table
-    clearTable();
-
- // create a click binding for TableButton
-    $('#clearTableButton').on('click', clearTable);
-
- // set up the 'Add Employee' button
-    $('#addEmployee').on('click', appendEmployeeForm);
-
- // delete button binding higher up in the DOM
-    $('table').on('click', 'deleteButton', handleDeleteButton);
-
-   });
-
-
-    function clearTable() {
-        $('tbody').empty();
-    }
-
-
-
-
-
-
-
-
-
-
+ 
 //create table
-////////////////function setup(){
+function setup(){
 //add inputs to the table
-//  const table= $(`
- //   <input id="employee-firstname" placeholder="Employee First Name" />
-  //  <input id="employee-lastname" placeholder="Employee Last Name" />
-  //  <input id="employee-IDnumber" placeholder="Employee ID Number" />
-  //  <input id="jobtitle" placeholder="Job Title" />
-  //  <input id="annual-salary" placeholder="Annual Salary" />
-  //  <button id="salary-submit">Add Salary</button>
-  //  <table id="salary-table">
-  //      <thead>
-  //          <tr>
-  //              <th>First Name</th>
-  //              <th>Last Name</th>
-  //              <th>ID Number</th>
-  //              <th>Job Title</th>
- //               <th>Annual Salary</th>
- //               <th>Delete</th>
- //           </tr>
- //      </thead>
- //      <tbody>
- //      </tbody>
- //  </table>
- //  <p id="salary-calculator">Total Annual Salary </p>
-//`);
-//add the table to the DOM
-//$('body').append(table);
-//add a button to calculate annual salary
-//$('#salary-submit').on('click', handleSalaryButton);
-//delete input
-//$('body').on('click','.deleteSalary', deleteRow);
-//}
+ const table= $(`
+ <input id="employee-firstname" placeholder="Employee First Name" />
+ <input id="employee-lastname" placeholder="Employee Last Name" />
+ <input id="employee-IDnumber" placeholder="Employee ID Number" />
+ <input id="jobTitle" placeholder="Job Title" />
+ <input id="annual-salary" placeholder="Annual Salary" />
+ <button id="salary-submit">Add Salary</button>
+   <table id="table">
+       <thead>
+           <tr>
+               <th>First Name</th>
+               <th>Last Name</th>
+               <th>ID Number</th>
+               <th>Job Title</th>
+               <th>Annual Salary</th>
+           </tr>
+      </thead>
+       <tbody>
+      </tbody>
+   </table>
+   <p id="monthly-calculator">Total Monthly Salary </p>
+`);
 
-////function handleSalaryButton(event) {
-//grab the first name
-///const firstname = $('#employee-firstname').val();
+//add the table to the DOM
+$('body').append(table);
+
+//call the employee-submit button
+$('#salary-submit').on('click', handleSalaryButton);
+
+//delete row button
+$(".deleteEmployee").on('click', ".delete.Employee", deleteRow);
+}
+
+function handleSalaryButton(event) {
+ //grab the first name
+const firstName = $('#employee-firstname').val();
 //grab the last name
-///const lastname = $('#employee-lastname').val();
+const lastName = $('#employee-lastname').val();
 //grab the employee ID
-//const ID = $('#employee-IDnumber').val();
+const ID = $('#employee-IDnumber').val();
 //grab the job title 
-//const jobtitle = $('#job-title').val();
+const jobTitle = $('#jobTitle').val();
 //grab the annual salary
-//const annualsalary = $("#annual-salary").val();
-//console.log(firstname);
-//console.log(lastname);
-//console.log(ID);
-//console.log(jobtitle);
-//console.log(annualsalary);
-//console.log(handleSalaryButton);
-//}
+const annualSalary = $('#annual-salary').val();
+console.log(firstName);
+console.log(lastName);
+console.log(ID);
+console.log(jobTitle);
+console.log(annualSalary);
+}
 
 //create a new row
-//const elem = $(`
-//<tr>
-//    <td>${"employee-firstname"}</td>
-//    <td>${"employee-lasttname"}</td>
-//    <td>${"employee-IDnumber"}</td>
-//    <td>${"jobtitle"}</td>
-//    <td>${"annualsalary"}</td>
-//    <td><button class="deleteAnnualsalary">Delete</button></td>
-// </tr> 
- //`);
+const elem = $(`
+<tr>
+  <td>${'firstName'}</td>
+  <td>${'lastName'}</td>  
+   <td>${'ID'}</td>
+    <td>${'jobTitle'}</td>
+    <td>${'annualSalary'}</td>
+    <td><button class="deleteEmployee">Delete</button></td>
+ </tr> 
+`);
 
 //clear the input values
-//$("#firstname").val('');
-//$("#lastname").val('');
-//$("#ID").val('');
-//$("#jobtitle").val('');
-//$("#annualsalary").val('');
+$("#firstname").val('');
+$("#lastname").val('');
+$("#ID").val('');
+$("#jobtitle").val('');
+$("#annualsalary").val('');
 
-//$("tbody").append(elem);
-//console.log(elem);
+$("table tbody").append(elem);
 
-//let totalAnnualSalary = 0;
+//update annual to montly cost
+function updateToMonthlySalary(){
 
-//totalAnnualSalary += Number('totalannualsalary');
-//console.log(totalAnnualSalary);
+}
+let totalAnnualSalary = 0;
+totalAnnualSalary += Number(annualSalary);
 
-//add function to delete each event after the button is clicked
-//function deleteRow(event) {
-//    console.log('In delete row');
 
-//let Deletebutton = $(event.target);
+$("#monthly-salary").text('Total Montlhy Salary $${monthlyCost}');
 
-//let targetRow = button.closest;
+////add total salary in DOM
+const annualSalary = $("#annual-salary").val();
 
-//targetRow.remove();
-//}
+
+///delete buttons
+function deleteRow(event) {
+   console.log('in delete row')
+}
+
+
